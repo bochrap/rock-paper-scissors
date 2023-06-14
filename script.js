fa211fee2a6fa7e5f072fdbd8c0fd4be81ae9330
bochrap <131978926+bochrap@users.noreply.github.com>
2023-06-14 21:41:51 +0200

    add function to play a single game

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random()*options.length)];
+    return options[Math.floor(Math.random()*options.length)].toLowerCase();
+}
+
+let playerSelection = prompt("Enter your selection:").toLowerCase();
+let computerSelection = getComputerChoice(); 
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
 }
