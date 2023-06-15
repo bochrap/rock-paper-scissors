b1d183a829eeacf7a3b287b5ca192f468f07913f
bochrap <131978926+bochrap@users.noreply.github.com>
2023-06-15 10:35:37 +0200

    add scoring to playRound()

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function getComputerChoice() {
 let playerSelection = "";
 let computerSelection = ""; 
 
+let round_n = 1;
+let user_score = 0;
+let computer_score = 0;
+
 function playRound(playerSelection, computerSelection) {
     
     playerSelection = prompt("Enter your selection:").toLowerCase();
@@ -16,14 +20,17 @@ function playRound(playerSelection, computerSelection) {
     }
 
     else if (playerSelection === "rock" && computerSelection === "paper") {
+        computer_score += 1;
         return "You lose! Paper beats rock :("
     }
 
     else if (playerSelection === "rock" && computerSelection === "scissors") {
+        user_score += 1;
         return "You win! Rock beats scissors"
     }
 
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        user_score += 1;
         return "You win! Paper beats rock!"
     }
 
@@ -32,14 +39,17 @@ function playRound(playerSelection, computerSelection) {
     }
 
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computer_score += 1;
         return "You lose! Scissors beat paper :("
     }
 
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computer_score += 1;
         return "You lose! Rock beats scissors :("
     }
 
     else if (playerSelection === "scissors" && computerSelection === "paper") {
+        user_score += 1;
         return "You win! Scissors beat paper!"
     }
 
@@ -52,5 +62,11 @@ function playRound(playerSelection, computerSelection) {
     }
 
     
+
+}
+
+
+
+function game() {
 
 }
